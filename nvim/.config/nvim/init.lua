0cf7ca5501e22ce8de5f3e751a3b9e22d3347f82
Stephan Kerkman <stephan@kerkman.io>
2026-02-09 12:30:08 +0100

    added nvim package

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,39 @@
+-- Bootstrap Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ 
+        "git", 
+        "clone", 
+        "--filter=blob:none", 
+        "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Set mappings correct for Lazy and further (vim.opt) settings
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup Lazy.nvim
+require("lazy").setup({
+    spec = {
+        -- import plugins
+        { import = "plugins" },
+    },
+
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
+})
+
+require("vim-options")
